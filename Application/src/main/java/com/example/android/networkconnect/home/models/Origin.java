fde8b0daec8977732757f12a006febf69cf1995b
Kevin Morales <kfmorales94@gmail.com>
2023-02-26 22:58:48 -0500

    API and models added

---
+++ Application/src/main/java/com/example/android/networkconnect/home/models/Origin.java
@@ -0,0 +1,26 @@
+package com.example.android.networkconnect.home.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Origin {
+    @SerializedName("url")
+    private String url;
+    @SerializedName("name")
+    private String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
